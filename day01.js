8d45a2fe29c6ed8dba2a96bdabfe02136f3eee0c
Marcin Floryan <mfloryan@spotify.com>
2018-12-01 14:32:45 +0100

    Day 01: part two done

--- day01.js
+++ day01.js
@@ -8,3 +8,25 @@ let listOfAdjustments = input.split('\n').map(n => parseInt(n));
 let result = listOfAdjustments.reduce((p,c) => p+c,0);
 
 console.log(result);
+
+let found = false;
+let frequency = 0;
+let frequencies = [];
+
+let iteration = 0;
+do {
+    iteration++;
+    console.log("Iteration: " + iteration);
+    for (let i = 0; i < listOfAdjustments.length; i++) {
+        const n = listOfAdjustments[i];
+        frequency += n;
+        if (frequencies.includes(frequency)) {
+            console.log("Found: " + frequency);
+            found = true;
+            break;
+        }
+        frequencies.push(frequency);
+    }
+    // if (iteration == 1) console.log(frequencies);
+    if (iteration%20 == 0) console.log(frequencies.length);
+} while (!found);
