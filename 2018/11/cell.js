e87af796f867070bd81000a185874ca7e9409a23
Marcin Floryan <mfloryan@spotify.com>
2018-12-11 06:40:30 +0100

    Day 11: :white_check_mark: :white_check_mark:

--- 2018/11/cell.js
+++ 2018/11/cell.js
@@ -1,6 +1,3 @@
-
-
-
 function cellPowerLevel(serial, coord) {
     let rackId = coord.x + 10;
     let power = rackId * coord.y;
@@ -11,36 +8,59 @@ function cellPowerLevel(serial, coord) {
     return power;
 }
 
-function powerOfSquare(serial, x, y) {
+function powerOfSquare(serial, x, y, size) {
     let sum = 0;
-    for (let i =0; i < 3; i++) {
-        for (let j=0; j< 3; j++) {
+    for (let i =0; i < size; i++) {
+        for (let j=0; j< size; j++) {
             sum+= cellPowerLevel(serial, {x: x+i, y:y+j});
         }
     }
     return sum;
 }
 
+function findFixedSizeMaxPowerSquare(serial, size) {
+    let maxPower = {p: -Infinity, x:0, y:0};
 
-let serial = 9110;
-let maxPower = {p:-Infinity, x:0, y:0};
-
-for (let i =1; i <= 300-3; i++) {
-    for (let j=1; j <= 300-3; j++) {
-        let p = powerOfSquare(serial, i, j);
-        if (p > maxPower.p) {
-            maxPower.p = p;
-            maxPower.x = i;
-            maxPower.y = j;
+    for (let i =1; i <= 300-size; i++) {
+        for (let j=1; j <= 300-size; j++) {
+            let p = powerOfSquare(serial, i, j, size);
+            if (p > maxPower.p) {
+                maxPower.p = p;
+                maxPower.x = i;
+                maxPower.y = j;
+            }
         }
     }
+    return maxPower;
 }
 
-console.log(maxPower);
+function findMaxPowerSquare(serial) {
+    let maxPower = {p:-Infinity, size: 0, x:0, y:0};
 
-console.log(cellPowerLevel(8, {x:3, y:5}));
-console.log(cellPowerLevel(57, {x:122, y:79}));
-console.log(cellPowerLevel(71, {x:101, y:153}));
+    for (let size=1; size <= 300; size++) {
+        // console.log("size: " + size);
+        let mps = findFixedSizeMaxPowerSquare(serial, size);
+        if (mps.p > maxPower.p) {
+            maxPower.p = mps.p;
+            maxPower.x = mps.i;
+            maxPower.y = mps.j;
+            maxPower.size = size;            
+        }
+    }
+    return maxPower;    
+}
 
-console.log(powerOfSquare(18,33,45))
-console.log(powerOfSquare(42,21,61))
+let serial = 9110;
+
+let mps1 = findFixedSizeMaxPowerSquare(serial,3);
+console.log(`Part one: ${mps1.x},${mps1.y}`);
+
+// let mps2 = findMaxPowerSquare(serial);
+// console.log(`Part two: ${mps2.x},${mps2.y},${mps2.size});
+
+console.log(cellPowerLevel(8, {x:3, y:5}) == 4);
+console.log(cellPowerLevel(57, {x:122, y:79}) == -5);
+console.log(cellPowerLevel(71, {x:101, y:153}) == 4);
+
+console.log(powerOfSquare(18,33,45,3) == 29);
+console.log(powerOfSquare(42,21,61,3) == 30);
