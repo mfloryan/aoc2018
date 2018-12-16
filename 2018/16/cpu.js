905535953ec52377bd70b3ac78df671c5727f64c
Marcin Floryan <mfloryan@spotify.com>
2018-12-16 19:37:12 +0100

    Day 16: part 2

--- 2018/16/cpu.js
+++ 2018/16/cpu.js
@@ -31,15 +31,15 @@ cpu.opcodes.forEach(o => o.action = o.action.bind(cpu));
 cpu.setRegisters = cpu.setRegisters.bind(cpu);
 
 function findMatchingOpCodes(input, instructions, output) {
-    let count = 0;
+    let matchingOpcodes = [];
     cpu.opcodes.forEach(op => {
         cpu.setRegisters(input);
         op.action(instructions[1], instructions[2], instructions[3]);
         if (output[instructions[3]] == cpu.registers[instructions[3]]) {
-            count++;
+            matchingOpcodes.push(op.name);
         }
     });
-    return count;
+    return matchingOpcodes;
 }
 
 let input0 = [
@@ -51,7 +51,7 @@ let input0 = [
 
 function parseInput(input) {
     let i = 0;
-    let counter = 0;
+    let opcodesMapping = [];
     while (input.length > 3) {
         if (input[0] == "") break;
         i++;
@@ -63,11 +63,53 @@ function parseInput(input) {
         after = after.substring(after.indexOf('[')+1, after.indexOf(']')).split(",").map(_ => parseInt(_));;
 
         let ops = findMatchingOpCodes(before, instruction, after);
-        if (ops >= 3) counter++;
+        opcodesMapping.push({id: instruction[0], ops: ops});
     }
-    return counter;
+    return opcodesMapping;
 }
 
-console.log(parseInput(input0) == 1);
+console.log(parseInput(input0).map(o => o.ops.length).filter(c => c >= 3).length == 1);
 
-console.log("Part 1: " + parseInput(input));
+let opcodesMapping = parseInput(input);
+console.log("Part 1: " + opcodesMapping.map(o => o.ops.length).filter(c => c >= 3).length);
+
+let candidates = {};
+
+opcodesMapping.forEach(m => {
+    if (!candidates[m.id]) candidates[m.id] = new Set();
+    m.ops.forEach(o => candidates[m.id].add(o));
+});
+
+do {
+    Object.keys(candidates).filter(k => candidates[k].size == 1).forEach(k => {
+        let instruction = [...candidates[k].values()][0];
+        Object.keys(candidates).forEach(key => {
+            if (k != key) {
+                candidates[key].delete(instruction);
+            }
+        });
+    });
+} while (Object.keys(candidates).map(k => candidates[k].size).filter(s => s > 1).length > 0);
+
+let map = {};
+Object.keys(candidates).forEach(k => map[k] = [...candidates[k]][0]);
+
+console.log(map);
+
+input.shift();
+input.shift();
+
+function executeCode(map, instructions) {
+    cpu.setRegisters([0, 0, 0, 0]);
+    instructions.forEach(i => {
+        instruction = map[i[0]];
+        let action = cpu.opcodes.find(op => op.name == instruction);
+        action.action(i[1], i[2], i[3]);
+    });
+    return cpu.registers;
+}
+
+let registers = executeCode(map, input.map(r => r.split(" ").map(_ => parseInt(_))));
+console.log(registers);
+
+console.log("Part 2: " + registers[0]);
