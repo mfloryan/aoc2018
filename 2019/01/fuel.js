81c06e4976990219592525c5beb10994d97397cf
Marcin Floryan <mfloryan@spotify.com>
2019-12-01 06:48:16 +0100

    2019: Lint JS code

--- 2019/01/fuel.js
+++ 2019/01/fuel.js
@@ -1,36 +1,36 @@
 const assert = require('assert');
 const fs = require('fs');
 
-console.log("Day 01");
+console.log('Day 01');
 
-let input = fs.readFileSync('input.txt', {encoding: 'utf8'});
-let modules = input.split('\n').map(n => parseInt(n));
+const input = fs.readFileSync('input.txt', { encoding: 'utf8' });
+const modules = input.split('\n').map(n => parseInt(n));
 
-function getFuel(mass) {
-    return Math.floor(mass / 3)  - 2;
+function getFuel (mass) {
+  return Math.floor(mass / 3) - 2;
 }
 
-assert.strictEqual(getFuel(12),2);
-assert.strictEqual(getFuel(14),2);
-assert.strictEqual(getFuel(1969),654);
-assert.strictEqual(getFuel(100756),33583);
+assert.strictEqual(getFuel(12), 2);
+assert.strictEqual(getFuel(14), 2);
+assert.strictEqual(getFuel(1969), 654);
+assert.strictEqual(getFuel(100756), 33583);
 
-let totalFuel = modules.map(m => getFuel(m)).reduce( (p,c) => p+c ,0);
+const totalFuel = modules.map(m => getFuel(m)).reduce((p, c) => p + c, 0);
 console.log(totalFuel);
 
-function getFuelFuel(mass) {
-    let fuels = [];
-    let lastFuel = getFuel(mass);
-    while (lastFuel > 0) {
-        fuels.push(lastFuel);
-        lastFuel = getFuel(lastFuel);
-    };
-    return fuels.reduce((a,c)=>a+c);
+function getFuelFuel (mass) {
+  const fuels = [];
+  let lastFuel = getFuel(mass);
+  while (lastFuel > 0) {
+    fuels.push(lastFuel);
+    lastFuel = getFuel(lastFuel);
+  };
+  return fuels.reduce((a, c) => a + c);
 }
 
-assert.strictEqual(getFuelFuel(14),2);
-assert.strictEqual(getFuelFuel(1969),966);
-assert.strictEqual(getFuelFuel(100756),50346);
+assert.strictEqual(getFuelFuel(14), 2);
+assert.strictEqual(getFuelFuel(1969), 966);
+assert.strictEqual(getFuelFuel(100756), 50346);
 
-let totalFuel2 = modules.map(m => getFuelFuel(m)).reduce( (p,c) => p+c);
+const totalFuel2 = modules.map(m => getFuelFuel(m)).reduce((p, c) => p + c);
 console.log(totalFuel2);
