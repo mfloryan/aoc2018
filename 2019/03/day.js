bd4b3f4f0f64bf1d462805130b80d82db3d4bf8e
Marcin Floryan <mfloryan@spotify.com>
2019-12-03 08:53:24 +0100

    Cleanup

--- 2019/03/day.js
+++ 2019/03/day.js
@@ -9,9 +9,6 @@ function mDistance (p1, p2) {
   return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
 }
 
-let w1 = 'R8,U5,L5,D3';
-let w2 = 'U7,R6,D4,L4';
-
 function parseWire (wire) {
   const parsedWire = wire.split(',');
   return parsedWire.map(w => {
@@ -24,26 +21,12 @@ function toCoordinates (parsedWire) {
   const coordinates = [{ ...start }];
 
   parsedWire.forEach(s => {
-    if (s.d == 'R') {
-      for (let i = 0; i < s.l; i++) {
-        start.x++;
-        coordinates.push({ ...start });
-      }
-    } else if (s.d == 'L') {
-      for (let i = 0; i < s.l; i++) {
-        start.x--;
-        coordinates.push({ ...start });
-      }
-    } else if (s.d == 'D') {
-      for (let i = 0; i < s.l; i++) {
-        start.y--;
-        coordinates.push({ ...start });
-      }
-    } else if (s.d == 'U') {
-      for (let i = 0; i < s.l; i++) {
-        start.y++;
-        coordinates.push({ ...start });
-      }
+    for (let i = 0; i < s.l; i++) {
+      if (s.d === 'R') start.x++;
+      else if (s.d === 'L') start.x--;
+      else if (s.d === 'D') start.y--;
+      else if (s.d === 'U') start.y++;
+      coordinates.push({ ...start });
     }
   });
 
@@ -51,31 +34,38 @@ function toCoordinates (parsedWire) {
 }
 
 function intersections (w1, w2) {
-  const i = w1.filter(p => w2.some(p2 => p.x == p2.x && p.y == p2.y));
+  const i = w1.filter(p => w2.some(p2 => p.x === p2.x && p.y === p2.y));
   return i;
 }
 
-// w1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72';
-// w2 = 'U62,R66,U55,R34,D71,R55,D58,R83';
+let w1 = 'R8,U5,L5,D3';
+let w2 = 'U7,R6,D4,L4';
+w1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72';
+w2 = 'U62,R66,U55,R34,D71,R55,D58,R83';
+
 w1 = input[0];
 w2 = input[1];
+
 const w1c = toCoordinates(parseWire(w1));
 const w2c = toCoordinates(parseWire(w2));
-
 const i = intersections(w1c, w2c).filter(p => p.x !== 0 && p.y !== 0);
 
-let ds = i.map(i => mDistance({ x: 0, y: 0 }, i));
-ds = ds.filter(p => p !== 0);
-ds.sort((a, b) => a - b);
+function findClosestIntersection (i) {
+  const ds = i.map(i => mDistance({ x: 0, y: 0 }, i));
+  ds.sort((a, b) => a - b);
+  return ds[0];
+}
 
-console.log(ds);
+console.log(findClosestIntersection(i));
 
-const d = i.map(i => {
-  const w1d = w1c.findIndex(p => p.x === i.x && p.y === i.y);
-  const w2d = w2c.findIndex(p => p.x === i.x && p.y === i.y);
-  console.log(`${w1d}, ${w2d} => ${w1d + w2d}`);
-  return w1d + w2d;
-});
+function findShortestPathIntersection (i, w1c, w2c) {
+  const d = i.map(i => {
+    const w1d = w1c.findIndex(p => p.x === i.x && p.y === i.y);
+    const w2d = w2c.findIndex(p => p.x === i.x && p.y === i.y);
+    return w1d + w2d;
+  });
+  d.sort((a, b) => a - b);
+  return d[0];
+}
 
-d.sort((a, b) => a - b);
-console.log(d);
+console.log(findShortestPathIntersection(i, w1c, w2c));
