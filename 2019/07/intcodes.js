3a6b980b913c64bbf72a8e3a1d50e55772af3dfa
Marcin Floryan <mfloryan@spotify.com>
2019-12-09 16:37:47 +0100

    2019:07 :white_check_mark::white_check_mark:

--- 2019/07/intcodes.js
+++ 2019/07/intcodes.js
@@ -22,7 +22,7 @@ class Cpu {
     },
     3: function input() {
       if (this.input.length < 1) {
-        console.log("Critical: input buffer empty");
+        return false;
       }
       let input = this.input.shift();
       this.memory[this.memory[this.pc+1]] = input;
@@ -72,19 +72,22 @@ class Cpu {
   }
 
   run () {
-    this.pc = 0;
     while (this.memory[this.pc] !== 99) {
 
       let opcodeValue = this.memory[this.pc] % 100;
       const op = this.opcodes[opcodeValue];
 
       if (op) {
-        op.apply(this);
+        if (op.apply(this) === false) {
+          return false;
+        }
       } else {
         console.log("Unknown opcode: " + op);
         break;
       }
     }
+
+    return true;
   }
 
   getMemory(address) {
@@ -145,9 +148,6 @@ assert.strictEqual(testPhaseSettings(sampleCode03, [1,0,4,3,2]), 65210);
 
 let inputCode = [3,8,1001,8,10,8,105,1,0,0,21,38,63,80,105,118,199,280,361,442,99999,3,9,102,5,9,9,1001,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,4,9,102,4,9,9,101,4,9,9,102,2,9,9,101,2,9,9,4,9,99,3,9,1001,9,5,9,102,4,9,9,1001,9,4,9,4,9,99,3,9,101,3,9,9,1002,9,5,9,101,3,9,9,102,5,9,9,101,3,9,9,4,9,99,3,9,1002,9,2,9,1001,9,4,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,99];
 
-console.log(testPhaseSettings(inputCode, [0,1,2,3,4]));
-console.log(testPhaseSettings(inputCode, [0,1,2,4,3]));
-
 function generatePermutations(length) {
   let sequence = [...Array(length).keys()];
   let permutations = [];
@@ -170,6 +170,68 @@ let ps = generatePermutations(5);
 let max = 0;
 ps.forEach(p => {
   max = Math.max(max, testPhaseSettings(inputCode, p));
+});
+
+console.log(max);
+assert.strictEqual(max, 118936);
+
+
+function testPhaseSettingsWithFeedback(code, phaseSettings) {
+
+  let cpus = [new Cpu(code), new Cpu(code), new Cpu(code), new Cpu(code), new Cpu(code)];
+
+  cpus[0].addInput(phaseSettings[0]);
+  cpus[1].addInput(phaseSettings[1]);
+  cpus[2].addInput(phaseSettings[2]);
+  cpus[3].addInput(phaseSettings[3]);
+  cpus[4].addInput(phaseSettings[4]);
+
+  cpus[0].addInput(0);
+
+  let run = false;
+
+  let result = 0;
+  do {
+    run = false;
+
+    let r1 = cpus[0].run();
+
+    cpus[1].addInput(cpus[0].getOutput().shift());
+    let r2 = cpus[1].run();
+
+    cpus[2].addInput(cpus[1].getOutput().shift());
+    let r3 = cpus[2].run();
+
+    cpus[3].addInput(cpus[2].getOutput().shift());
+    let r4 = cpus[3].run();
+
+    cpus[4].addInput(cpus[3].getOutput().shift());
+    let r5 = cpus[4].run();
+    result = cpus[4].getOutput()[0];
+
+    cpus[0].addInput(cpus[4].getOutput().shift());
+    run = !r1 || !r2 || !r3 || !r4 || !r5;
+  } while (run);
+
+  return result;
+}
+
+
+let sampleCodeB01 = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5];
+assert.strictEqual(testPhaseSettingsWithFeedback(sampleCodeB01,[9,8,7,6,5]) , 139629729);
+
+let sampleCodeB02 = [
+  3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+  -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+  53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10];
+assert.strictEqual(testPhaseSettingsWithFeedback(sampleCodeB02,[9,7,8,5,6]), 18216);
+
+let ps2 = generatePermutations(5);
+
+ps2 = ps2.map(p => p.map(n => n+5));
+
+ps2.forEach(p => {
+  max = Math.max(max, testPhaseSettingsWithFeedback(inputCode, p));
 });
 
 console.log(max);
