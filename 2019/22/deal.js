8dc74e729aed44bdcf5865f1d464befbb705801f
Marcin Floryan <mfloryan@spotify.com>
2019-12-22 12:53:19 +0100

    2019-22: part one improved

--- 2019/22/deal.js
+++ 2019/22/deal.js
@@ -4,16 +4,18 @@ const assert = require('assert');
 
 const input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8' });
 
-function parseInput (input) {
+function parseInput (input, step2 = false) {
   const shuffleFunctions = [];
   const instructions = input.split('\n');
   instructions.forEach(i => {
-    if (i === 'deal into new stack') shuffleFunctions.push(dealIntoNewStack);
+    if (i === 'deal into new stack') shuffleFunctions.push(step2 ? getIndexOfdealIntoNewStack : dealIntoNewStack);
     if (i.startsWith('cut')) {
-      shuffleFunctions.push(d => cutNcard(parseInt(i.split(' ')[1]), d));
+      const n = parseInt(i.split(' ')[1]);
+      shuffleFunctions.push(step2 ? getIndexOfcutNcard.bind(null, n) : d => cutNcard(n, d));
     }
     if (i.startsWith('deal with increment')) {
-      shuffleFunctions.push(d => dealWithIncrement(parseInt(i.split(' ')[3]), d));
+      const n = parseInt(i.split(' ')[3]);
+      shuffleFunctions.push(step2 ? getIndexOfdealWithIncrement.bind(null, n) : d => dealWithIncrement(n, d));
     }
   });
   return shuffleFunctions;
@@ -31,6 +33,18 @@ function cutNcard (n, deck) {
   return deck.slice(n).concat(deck.slice(0, n));
 }
 
+function getIndexOfdealIntoNewStack(index, length) {
+  return (length - index - 1);
+}
+
+function getIndexOfcutNcard(n, index, length) {
+  return ((index - n) + length) % length;
+}
+
+function getIndexOfdealWithIncrement(increment, index, length) {
+  return ((index * increment) % length);
+}
+
 function dealWithIncrement (increment, deck) {
   const newDeck = Array(deck.length);
   deck.forEach((n, i) => { newDeck[(i * increment) % deck.length] = n; });
@@ -45,7 +59,15 @@ function shuffle (deck, instructions) {
   return thisDeck;
 }
 
-const f = parseInput(`deal into new stack
+function trackCard(index, length, instructions) {
+  let thisIndex = index;
+  instructions.forEach(i => {
+    thisIndex = i(thisIndex, length);
+  });
+  return thisIndex;
+}
+
+const example = `deal into new stack
 cut -2
 deal with increment 7
 cut 8
@@ -54,13 +76,16 @@ deal with increment 7
 cut 3
 deal with increment 9
 deal with increment 3
-cut -1`);
+cut -1`;
 
 assert.strictEqual(dealWithIncrement(3, getDeck(10)).join(' '), '0 7 4 1 8 5 2 9 6 3');
 assert.strictEqual(cutNcard(3, getDeck(10)).join(' '), '3 4 5 6 7 8 9 0 1 2');
 assert.strictEqual(cutNcard(-4, getDeck(10)).join(' '), '6 7 8 9 0 1 2 3 4 5');
 
-assert.strictEqual(shuffle(getDeck(10), f).join(' '), '9 2 5 8 1 4 7 0 3 6');
+assert.strictEqual(shuffle(getDeck(10), parseInput(example)).join(' '), '9 2 5 8 1 4 7 0 3 6');
 
 const shuffledDeck = shuffle(getDeck(10007), parseInput(input));
 console.log(shuffledDeck.findIndex(c => c === 2019));
+console.log(trackCard(2019, 10007, parseInput(input, true)));
+
+// part 2
